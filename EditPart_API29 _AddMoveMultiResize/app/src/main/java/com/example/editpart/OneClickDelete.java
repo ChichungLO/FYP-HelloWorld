804336df2fc2fb77d6e3f83490795ed1e8d6c134
leo <s1230481@ouhk.edu.hk>
2021-05-06 11:21:11 +0800

    图像二值化

--- EditPart_API29 _AddMoveMultiResize/app/src/main/java/com/example/editpart/OneClickDelete.java
+++ EditPart_API29 _AddMoveMultiResize/app/src/main/java/com/example/editpart/OneClickDelete.java
@@ -11,7 +11,11 @@ import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -142,7 +146,7 @@ public class OneClickDelete extends AppCompatActivity {
                     //imageView.setImageBitmap(bm);
                     Bitmap newBitmap = clearBlue(bm, Color.argb(255, 255, 255, 255));
                     imageView.setImageDrawable(null);
-                    imageView.setImageBitmap(newBitmap);
+                    imageView.setImageBitmap(BinaryGray(newBitmap));
                     Toast toast = Toast.makeText(getApplicationContext(), R.string.ClearSuccess, Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER,0,-100);
                     toast.show();
@@ -163,7 +167,7 @@ public class OneClickDelete extends AppCompatActivity {
                     //imageView.setImageBitmap(bm);
                     Bitmap newBitmap = clearRed(bm, Color.argb(255, 255, 255, 255));
                     imageView.setImageDrawable(null);
-                    imageView.setImageBitmap(newBitmap);
+                    imageView.setImageBitmap(BinaryGray(newBitmap));
                     Toast toast = Toast.makeText(getApplicationContext(), R.string.ClearSuccess, Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER,0,-100);
                     toast.show();
@@ -331,6 +335,41 @@ public class OneClickDelete extends AppCompatActivity {
             }
         }
         return new int[]{maxR*16,maxG*16,maxB*16};
+    }
+
+    public Bitmap BinaryGray(Bitmap bm) {
+        //得到图形的宽度和长度
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        //创建二值化图像
+        Bitmap binarymap = null;
+        binarymap = bm.copy(Bitmap.Config.ARGB_8888, true);
+        //依次循环，对图像的像素进行处理
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                //得到当前像素的值
+                int col = binarymap.getPixel(i, j);
+                //得到alpha通道的值
+                int alpha = col & 0xFF000000;
+                //得到图像的像素RGB的值
+                int red = (col & 0x00FF0000) >> 16;
+                int green = (col & 0x0000FF00) >> 8;
+                int blue = (col & 0x000000FF);
+                // 用公式X = 0.3×R+0.59×G+0.11×B计算出X代替原来的RGB
+                int gray = (int) ((float) red * 0.3 + (float) green * 0.59 + (float) blue * 0.11);
+                //对图像进行二值化处理
+                if (gray <= 95) {
+                    gray = 0;
+                } else {
+                    gray = 255;
+                }
+                // 新的ARGB
+                int newColor = alpha | (gray << 16) | (gray << 8) | gray;
+                //设置新图像的当前像素值
+                binarymap.setPixel(i, j, newColor);
+            }
+        }
+        return binarymap;
     }
 
 //    private void handleImage(Intent data) {
